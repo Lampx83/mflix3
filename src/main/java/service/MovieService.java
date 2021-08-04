3bd38405c708a734856d2d46ebe6368d1b2f9a4f
xuanlam <phamxuanlam@gmail.com>
2021-08-04 15:51:12 +0700

    Update

--- src/main/java/service/MovieService.java
+++ src/main/java/service/MovieService.java
@@ -10,29 +10,10 @@ import java.util.List;
 
 public class MovieService {
 
-    public List<Movie> getMoviesforHomePage() {
-        List<Movie> list = new MovieDAO().getMovies(6);
-        if (list == null) {
-            list = new ArrayList<>();
-            //add some sample movies;
-        }
-        return list;
-    }
 
     public Movie getMovieByID(String id) {
         Movie movie = new MovieDAO().getMovieByID(id);
         return movie;
-    }
-
-    public DistinctIterable<String> getGenres() {
-        DistinctIterable<String> list = new MovieDAO().getGenres();
-        return list;
-    }
-
-    public List<String> getGenresforHeader() {
-        ArrayList<String> list = new ArrayList<>();
-        new MovieDAO().getTopGenres(15).forEach(d -> list.add(d.getString("_id")));
-        return list;
     }
 
     final static int NUM_OF_MOVIE_ON_PAGE = 6;
